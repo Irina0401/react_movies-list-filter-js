80abe2111c9a8500cd310757f7e174605905db2f
Irina <weedfox053@gmail.com>
2025-08-16 23:32:55 +0300

    react movies list filter

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import moviesFromServer from './api/movies.json';
 export const App = () => {
   const [query, setQuery] = useState('');
 
- const handleChange = (e) => {
-  setQuery(e.target.value)
- }
- const result = query.trim().toLowerCase();
+  const handleChange = e => {
+    setQuery(changeEvent.target.value);
+  };
 
+  const result = query.trim().toLowerCase();
 
   const visibleMovies = moviesFromServer.filter(movie => {
-    if(!result) return true;
+    if (!result) return true;
 
     const title = String(movie.title || '').toLowerCase();
     const descript = String(movie.description || '').toLowerCase();
@@ -21,33 +21,33 @@ export const App = () => {
     return title.includes(result) || descript.includes(result);
   });
 
-return (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
 
-          <div className="control">
-            <input
-              type="text"
-              value={saveInput}
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-              onChange = {handleChange}
-            />
+            <div className="control">
+              <input
+                type="text"
+                value={query}
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={handleChange}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={visibleMovies} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
 };
